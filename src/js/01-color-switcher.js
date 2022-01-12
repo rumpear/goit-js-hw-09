b854e8d16000f1be9053d197d0b34b7f0b2160ae
Roman <77742141+rumpear@users.noreply.github.com>
2022-01-12 18:22:07 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartClick);
+
+function onStartClick() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtn.disabled = true;
+}
+
+stopBtn.addEventListener('click', onStopClick);
+
+function onStopClick() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
